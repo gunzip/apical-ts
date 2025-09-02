472317d7bee9678a7a15c035482395b6f4441ad5
Danilo Spinelli <danilo.spinelli@pagopa.it>
2025-09-02 14:56:11 +0000

    Update website content and styles: enhance introduction, refine CSS for sections, and improve Docusaurus configuration

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "@apical-ts/craft",
-  tagline: "Yet Another OpenAPI to TypeScript Generator",
+  tagline: "Strictly-typed client and server from OpenAPI to Typescript",
   favicon: "img/favicon.ico",
 
   headTags: [
